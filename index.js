eae3e7995358c276ae3e7f4c5a255acb522ac657
Bsanika <sbidkar127@gmail.com>
2024-06-17 15:25:44 +0530

    Update index.html, styles.css, and index.js with new modifications

--- index.js
+++ index.js
@@ -70,21 +70,25 @@ window.onscroll=()=>{
       clickable: true,
     },
     breakpoints: {
+      300:{
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
         450:{
         slidesPerView: 2,
         spaceBetween: 20,
         },
       540: {
-        slidesPerView: 4,
+        slidesPerView: 3,
         spaceBetween: 20,
       },
       768: {
-        slidesPerView: 4,
-        spaceBetween: 50,
+        slidesPerView: 3,
+        spaceBetween: 20,
       },
       1024: {
-        slidesPerView: 4,
-        spaceBetween: 50,
+        slidesPerView: 3,
+        spaceBetween: 20,
       },
     },
   });
